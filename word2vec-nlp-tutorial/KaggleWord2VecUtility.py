89c82ffaa7075620449e7eb9bccf73ece4d0cc8d
corazzon <joeunpark@gmail.com>
2018-01-11 17:31:43 +0900

    add KaggleWord2VecUtility

---
+++ word2vec-nlp-tutorial/KaggleWord2VecUtility.py
@@ -0,0 +1,78 @@
+import re
+import nltk
+
+import pandas as pd
+import numpy as np
+
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+from multiprocessing import Pool
+
+class KaggleWord2VecUtility(object):
+
+
+    @staticmethod
+    def review_to_wordlist(review, remove_stopwords=False):
+        # 1. HTML 제거
+        review_text = BeautifulSoup(review, "html.parser").get_text()
+        # 2. 특수문자를 공백으로 바꿔줌
+        review_text = re.sub('[^a-zA-Z]', ' ', review_text)
+        # 3. 소문자로 변환 후 나눈다.
+        words = review_text.lower().split()
+        # 4. 불용어 제거
+        if remove_stopwords:
+            stops = set(stopwords.words('english'))
+            words = [w for w in words if not w in stops]
+        # 5. 어간추출
+        stemmer = SnowballStemmer('english')
+        words = [stemmer.stem(w) for w in words]
+        # 6. 리스트 형태로 반환
+        return(words)
+
+
+    @staticmethod
+    def review_to_sentences( review, remove_stopwords=False ):
+        # punkt tokenizer를 로드한다.
+        """
+        이 때, pickle을 사용하는데
+        pickle을 통해 값을 저장하면 원래 변수에 연결 된 참조값 역시 저장된다.
+        저장된 pickle을 다시 읽으면 변수에 연결되었던
+        모든 레퍼런스가 계속 참조 상태를 유지한다.
+        """
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        # 1. nltk tokenizer를 사용해서 단어로 토큰화 하고 공백 등을 제거한다.
+        raw_sentences = tokenizer.tokenize(review.strip())
+        # 2. 각 문장을 순회한다.
+        sentences = []
+        for raw_sentence in raw_sentences:
+            # 비어있다면 skip
+            if len(raw_sentence) > 0:
+                # 태그제거, 알파벳문자가 아닌 것은 공백으로 치환, 불용어제거
+                sentences.append(\
+                    KaggleWord2VecUtility.review_to_wordlist(\
+                    raw_sentence, remove_stopwords))
+        return sentences
+
+
+    # 참고 : https://gist.github.com/yong27/7869662
+    # http://www.racketracer.com/2016/07/06/pandas-in-parallel/
+    # 속도 개선을 위해 멀티 스레드로 작업하도록
+    @staticmethod
+    def _apply_df(args):
+        df, func, kwargs = args
+        return df.apply(func, **kwargs)
+
+    @staticmethod
+    def apply_by_multiprocessing(df, func, **kwargs):
+        # 키워드 항목 중 workers 파라메터를 꺼냄
+        workers = kwargs.pop('workers')
+        # 위에서 가져온 workers 수로 프로세스 풀을 정의
+        pool = Pool(processes=workers)
+        # 실행할 함수와 데이터프레임을 워커의 수 만큼 나눠 작업
+        result = pool.map(KaggleWord2VecUtility._apply_df, [(d, func, kwargs)
+                for d in np.array_split(df, workers)])
+        pool.close()
+        # 작업 결과를 합쳐서 반환
+        return pd.concat(result)
